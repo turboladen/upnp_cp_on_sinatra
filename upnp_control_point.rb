e0844040ccbf800d4128eb64b4f219d0d82796be
Steve Loveless <steve.loveless@gmail.com>
2012-09-27 01:31:07 -0700

    Added route to single device

--- upnp_control_point.rb
+++ upnp_control_point.rb
@@ -10,4 +10,10 @@ class UPnPControlPoint < Sinatra::Base
     @friendly_names = @devices.map(&:friendly_name)
     haml :index
   end
+
+  get '/:usn' do
+    @device = env['upnp.devices'].find { |d| d.usn == params[:usn] }
+    puts "found device: #{@device}"
+    haml :device
+  end
 end
